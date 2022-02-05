389b904cf463404437318865dbb896280bb8aa25
Soro_pz45@itstepru.onmicrosoft.com <bu_ha_ha@bk.ru>
2022-02-05 20:47:21 +0300

    Add project files.

---
+++ Money.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Lab
+{
+    public delegate double MoneyConvertDelegate(double value);
+    class Money
+    {
+        private string CurrencyCode { get; set; }
+        private double Amount { get; set; }
+         public Money (double amount, string currenyCode)
+        {
+            Amount = amount;
+            CurrencyCode = currenyCode;
+        }
+
+        public override string ToString()
+        {
+            return $"Баланс составляет {Amount} {CurrencyCode}";
+        }
+
+        public void Convert (MoneyConvertDelegate del)
+        {
+            foreach (MoneyConvertDelegate item in del.GetInvocationList())
+            {
+                Console.Write("Курс в пересчете составил: ");
+                item(Amount);
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Money currency = new Money(20, "руб");
+            MoneyConvertDelegate conv = null;
+
+            Console.WriteLine(currency);
+            conv += delegate (double amount)
+            {
+                Console.WriteLine($"{ amount} руб = { amount*0.013} дол");
+                return amount;
+            };
+
+            conv += delegate (double amount)
+            {
+                Console.WriteLine($"{ amount} руб = { amount * 0.084} юаней");
+                return amount;
+            };
+
+            conv += delegate (double amount)
+            {
+                Console.WriteLine($"{ amount} руб = { amount * 5.70} тенге");
+                return amount;
+            };
+
+            conv += delegate (double amount)
+            {
+                Console.WriteLine($"{ amount} руб = { amount * 15.82} вон");
+                return amount;
+            };
+            
+            conv += (double amount) =>
+            {
+                Console.WriteLine($"{ amount} руб = { amount * 0.000007} в золотишке");
+                return amount;
+            };
+            currency.Convert(conv);
+        }
+    }
+}
